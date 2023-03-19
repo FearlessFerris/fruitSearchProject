851ba96c5da1d259d3515c5982359acdb9c7ebc7
Marcus Perry <marcus.perry2017@gmail.com>
2023-03-18 20:05:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+input.addEventListener('keyup', function() {
+	let results = [];
+	let inputs = input.value;
+
+	if (inputs.length) {
+		results = fruit.filter(function (names) {
+			return names.toLowerCase().includes(inputs.toLowerCase());
+		})
+
+		console.log(results);
+	}
+
+	showSuggestions(results);
+
+	if (!results.length){
+		suggestions.innerHTML = '';
+	}
+})
+
+function searchHandler(e) {
+	
+}
+
+function showSuggestions(results){
+
+	const show = results.map(function(list){
+		return '<li onclick=useSuggestion(this)>' + list + '</li>';
+	})
+
+	suggestions.innerHTML = '<ul>' + show.join('') + '<ul>';
+}
+
+function useSuggestion(list) {
+	input.value = list.innerHTML;
+	suggestions.innerHTML = '';
+	
+}
+
